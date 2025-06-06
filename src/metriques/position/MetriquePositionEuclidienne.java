f6a61c2b4d673f89348ddf7040721f2d09d125da
Yeahboys04 <ryankorban2004@gmail.com>
2025-06-06 12:59:53 +0200

    Ajout MetriquePositionEuclidienne.java

---
+++ src/metriques/position/MetriquePositionEuclidienne.java
@@ -0,0 +1,23 @@
+package metriques.position;
+
+import metriques.MetriqueDistance;
+import donnees.PixelData;
+
+/**
+ * Métrique de distance euclidienne pour les positions de pixels.
+ */
+public class MetriquePositionEuclidienne implements MetriqueDistance<PixelData> {
+
+    @Override
+    public double calculerDistance(PixelData pixel1, PixelData pixel2) {
+        double dx = pixel1.getX() - pixel2.getX();
+        double dy = pixel1.getY() - pixel2.getY();
+        return Math.sqrt(dx * dx + dy * dy);
+    }
+
+    @Override
+    public String getNom() {
+        return "Distance Euclidienne - Position";
+    }
+}
+
