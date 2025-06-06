ffdf418d3a78ac8e8385efbffaa8dbf7b35d28c9
Yeahboys04 <ryankorban2004@gmail.com>
2025-06-06 13:00:03 +0200

    Ajout MetriqueCouleur.java

---
+++ src/metriques/couleur/MetriqueCouleur.java
@@ -0,0 +1,28 @@
+package metriques.couleur;
+
+import metriques.MetriqueDistance;
+import donnees.PixelData;
+import normeCouleurs.NormeCouleurs;
+import java.awt.Color;
+
+/**
+ * Adaptateur qui permet d'utiliser les normes de couleurs existantes
+ * comme métriques génériques sur des PixelData.
+ */
+public class MetriqueCouleur implements MetriqueDistance<PixelData> {
+    private final NormeCouleurs normeCouleur;
+
+    public MetriqueCouleur(NormeCouleurs normeCouleur) {
+        this.normeCouleur = normeCouleur;
+    }
+
+    @Override
+    public double calculerDistance(PixelData pixel1, PixelData pixel2) {
+        return normeCouleur.distanceCouleur(pixel1.getCouleur(), pixel2.getCouleur());
+    }
+
+    @Override
+    public String getNom() {
+        return normeCouleur.getNom() + " - RGB";
+    }
+}
